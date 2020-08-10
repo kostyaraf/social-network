0b8bf980f807915c6aba64a331fdfcb8577b2d83
k.rafalsky <k.rafalsky@splinestudio.ua>
2020-08-10 22:43:18 +0300

    [01] replace filed Header and Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-const Navbar = () => (
-  <nav className='nav'>
-    <div><a href='#'>list 1</a></div>
-    <div><a href='#'>list 2</a></div>
-    <div><a href='#'>list 3</a></div>
-    <div><a href='#'>list 4</a></div>
-    <div><a href='#'>list 5</a></div>
-  </nav>
+const Header = () => (
+  <header className='header'>
+    <img src='/logo192.png' />
+    I'm Header
+  </header>
 )
+export default Header
 
-export default Navbar
