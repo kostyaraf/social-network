0f445b46c50952d9831339427f180ee539789545
k.rafalsky <k.rafalsky@splinestudio.ua>
2020-08-10 22:30:09 +0300

    [01] cut first app on a components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header className='header'>
+    <img src='/logo192.png' />
+    I'm Header
+  </header>
+)
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className='content'>
+    I'm Content
+    <div>
+      <img
+        src='https://multiurok.ru/img/302295/image_592609a602d7e.jpg'
+        alt=''
+      />
+    </div>
+    <div>ava + descrip</div>
+    <div>
+      My post
+      <div>New Posts</div>
+      <div>post 1</div>
+      <div>post 2</div>
+      <div>post 3</div>
+    </div>
+  </div>
+)
+
+export default Profile
